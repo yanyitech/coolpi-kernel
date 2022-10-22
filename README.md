a0829a170174e847c0944c223cee639a4b6606fd
jack <jack@yanqinteck.com>
2022-10-22 11:51:12 +0800

    add script for generate coolpi boot img

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Rockchip Cool Pi RK3588S Board
+Rockchip Cool Pi 4 Model B Board
 
 论坛：https://www.cool-pi.com
 
@@ -6,5 +6,8 @@ Rockchip Cool Pi RK3588S Board
 ./build-kernel.sh
 
 编译成功，在out目录下有三个文件，拷贝到主板上重启生效（驱动包需要解压）
+
+打包coolpi-boot.img
+sudo ./build-fatboot.sh
 
 
---
+++ build-fatboot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Create coolpi boot img..."
+dd if=/dev/zero of=coolpi-boot.img bs=1M count=300
+mkfs.vfat -F 32 coolpi-boot.img
+fatlabel coolpi-boot.img system-boot
+mkdir -p .temp
+mount coolpi-boot.img .temp/
+cp out/* .temp/
+cp boot/* .temp/
+umount .temp
+echo "Complete..."
